e4c04f2519fc813549bb8c439088791e8adc68b2
Alexandr Tatarnikov <colmoon@ya.ru>
2020-07-24 15:27:17 +0300

    change .filter > .find, error correction eslint

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,23 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
-const users = require('../data/users.json')
+const users = require('../data/users.json');
 
 router.get('/users', (req, res) => {
-  const usersPath = path.join(__dirname, '../data', 'users.json')
-  res.set({'content-type': 'application/json; charset=utf-8'});
-  fs.createReadStream(usersPath, 'utf-8').pipe(res)
-})
+  const usersPath = path.join(__dirname, '../data', 'users.json');
+  res.set({ 'content-type': 'application/json; charset=utf-8' });
+  fs.createReadStream(usersPath, 'utf-8').pipe(res);
+});
 
 router.get('/users/:id', (req, res) => {
-  const {id} = req.params
-  const usersList = [...users]
-  const user = []
-  usersList.filter((item) => {
-    if (item._id === id) {
-      user.push(item)
-      return
-    }
-  })
-  if (Object.keys(user).length === 0) {
-    res.status(404).send({'message': 'Нет пользователя с таким id'})
-  } else {
-    res.send(user)
+  const { id } = req.params;
+  const usersList = [...users];
+  const user = usersList.find((person) => person._id === id);
+  if (!user) {
+    res.status(404).send({ message: 'Нет пользователя с таким id' });
+    return;
   }
-})
+  res.send(user);
+});
 
-module.exports = router
+module.exports = router;
