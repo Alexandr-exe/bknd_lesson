11e5e3fd83d6628699bcdcb911a84737ce644f1a
Alexandr Tatarnikov <colmoon@ya.ru>
2020-07-24 10:39:49 +0300

    change status 400 > 404

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', cards)
 app.use('/', users)
 app.use(function (req, res) {
-  res.status(400).send({"message": "Запрашиваемый ресурс не найден"})
+  res.status(404).send({"message": "Запрашиваемый ресурс не найден"})
 })
 
 app.listen(PORT)
