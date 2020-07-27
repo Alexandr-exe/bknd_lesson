350459559ff8fd28e5ea7a4be8cbb94b9b4c960b
Alexandr Tatarnikov <colmoon@ya.ru>
2020-07-27 13:58:08 +0300

    add error processing

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,40 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
-const users = require('../data/users.json');
+
+const usersPath = path.join(__dirname, '../data', 'users.json');
+
+const usersReadFile = (filePath) => {
+  const file = fs.createReadStream(filePath, { encoding: 'utf8' });
+  return file;
+};
 
 router.get('/users', (req, res) => {
-  const usersPath = path.join(__dirname, '../data', 'users.json');
+  const file = usersReadFile(usersPath);
+  file.on('error', () => {
+    res.set({ 'content-type': 'application/json; charset=utf-8' });
+    res.status(500).send({ message: 'Что то пошло не так' });
+  });
   res.set({ 'content-type': 'application/json; charset=utf-8' });
-  fs.createReadStream(usersPath, 'utf-8').pipe(res);
+  file.pipe(res);
 });
 
 router.get('/users/:id', (req, res) => {
   const { id } = req.params;
-  const user = users.find((person) => person._id === id);
-  if (!user) {
-    res.status(404).send({ message: 'Нет пользователя с таким id' });
-    return;
-  }
-  res.send(user);
+  const users = usersReadFile(usersPath);
+  users.on('error', () => {
+    res.status(500).send({ message: 'Ошибка Сервера' });
+  });
+  users.on('data', (data) => {
+    const usersList = JSON.parse(data);
+    res.set({ 'content-type': 'application/json; charset=utf-8' });
+    const user = usersList.find((person) => person._id === id);
+    if (!user) {
+      res.status(404).send({ message: 'Пользователь с таким id не найдён' });
+      return;
+    }
+    res.send(user);
+  });
 });
 
 module.exports = router;
