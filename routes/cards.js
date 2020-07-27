7dcd236a9076f031d1b06e5266048d5f59001a1e
Alexandr Tatarnikov <colmoon@ya.ru>
2020-07-27 16:59:01 +0300

    add error processing

--- routes/cards.js
+++ routes/cards.js
@@ -5,8 +5,13 @@ const fs = require('fs');
 const cardsPath = path.join(__dirname, '../data', 'cards.json');
 
 router.get('/cards', (req, res) => {
+  const file = fs.createReadStream(cardsPath);
+  file.on('error', () => {
+    res.set({ 'content-type': 'application/json; charset=utf-8' });
+    res.status(500).send({ message: 'Что то пошло не так' });
+  });
   res.set({ 'content-type': 'application/json; charset=utf-8' });
-  fs.createReadStream(cardsPath).pipe(res);
+  file.pipe(res);
 });
 
 module.exports = router;
